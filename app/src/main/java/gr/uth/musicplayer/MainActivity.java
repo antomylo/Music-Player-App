0b333c01b45b4c55564d632984b062fca3f66161
Alexandros Voliotis <alexvoliotis45@gmail.com>
2022-12-11 13:16:44 +0200

    some improvements (only gets songs from the first folder)

--- app/src/main/java/gr/uth/musicplayer/MainActivity.java
+++ app/src/main/java/gr/uth/musicplayer/MainActivity.java
@@ -18,12 +18,12 @@ import android.widget.ListView;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView listView;
-    String[] items;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,30 +56,68 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public ArrayList<File> findSong (File file){
-        ArrayList<File> arrayList = new ArrayList<>();
-        File[] files = file.listFiles();
+//    public ArrayList<File> findSong (File file){
+//        ArrayList<File> arrayList = new ArrayList<>();
+//        File[] files = file.listFiles();
+//
+//        for (File singlefile: files) {
+//            if (singlefile.isDirectory() && !singlefile.isHidden()) {
+//                arrayList.addAll(findSong(singlefile));
+//            } else {
+//                if (singlefile.getName().endsWith(".mp3") || singlefile.getName().endsWith(".wav")) {
+//                    arrayList.add(singlefile);
+//                }
+//            }
+//        }
+//        return arrayList;
+//    }
 
-        for (File singlefile: files) {
-            if (singlefile.isDirectory() && !singlefile.isHidden()) {
-                arrayList.addAll(findSong(singlefile));
-            } else {
-                if (singlefile.getName().endsWith(".mp3") || singlefile.getName().endsWith(".wav")) {
-                    arrayList.add(singlefile);
+    private List<File> findSongsFolders(File rootFolder) {
+        List<File> songFolders = new ArrayList<>();
+        for (File file : rootFolder.listFiles()) {
+            if (file.isDirectory() && !file.isHidden()) {
+                if (file != null) {
+                    for (File insideFile : file.listFiles()) {
+                        if (insideFile.getName().endsWith(".mp3") || insideFile.getName().endsWith("wav")) {
+                            songFolders.add(file);
+                        }
+                    }
                 }
             }
         }
-        return arrayList;
+        return songFolders;
+    }
+
+    private List<File> findSongs(List<File> songFolders) {
+        if (songFolders == null || songFolders.size() == 0) return null;
+        List<File> songs = new ArrayList<>();
+        for (File folder : songFolders) {
+            if (folder.isDirectory() && !folder.isHidden()) {
+                for (File song : folder.listFiles()) {
+                    if (song.getName().endsWith(".mp3") || song.getName().endsWith("wav")) {
+                        songs.add(song);
+                    }
+                }
+            }
+        }
+        return songs;
     }
 
     void displaySongs(){
-        final ArrayList<File> mySongs = findSong(Environment.getExternalStorageDirectory());
+//        final ArrayList<File> mySongs = findSong(Environment.getExternalStorageDirectory());
+        final List<File> mySongs = findSongs(findSongsFolders(Environment.getExternalStorageDirectory()));
 
-        items = new String[mySongs.size()];
-        for(int i = 0; i < mySongs.size(); i++){
-            items[i] = mySongs.get(i).toString().replace(".mp3", "").replace(".wav", "");
+        if (mySongs != null && mySongs.size() > 0) {
+            String[] items = new String[mySongs.size()];
+            for(int i = 0; i < mySongs.size(); i++){
+                items[i] = mySongs.get(i).toString()
+                        .substring(mySongs.get(i).toString().lastIndexOf("/")+1)
+                        .replace(".mp3", "").replace(".wav", "");
+            }
+            ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+            listView.setAdapter(myAdapter);
+        } else {
+            // todo: implement later
         }
-        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
-        listView.setAdapter(myAdapter);
     }
 }
